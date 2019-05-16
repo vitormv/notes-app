f61ebd4310c39e19081eba6b5b97937c0090c6bd
Vitor Mello <vitor.mello@flixbus.com>
2019-05-16 11:46:06 +0200

    Fix complaints by scrutinizer yml

--- src/components/TextEditor/getMarkdownBlockFromPrefix.js
+++ src/components/TextEditor/getMarkdownBlockFromPrefix.js
@@ -8,32 +8,25 @@ import {
     BLOCK_SEPARATOR,
 } from 'src/components/TextEditor/SlateDictionary';
 
-const getMarkdownBlockFromPrefix = (chars) => {
-    switch (chars) {
-        case '*':
-        case '-':
-        case '+':
-            return BLOCK_LIST_ITEM;
-        case '>':
-            return BLOCK_QUOTE;
-        case '#':
-        case 'h1.':
-            return BLOCK_H1;
-        case '##':
-        case 'h2.':
-            return BLOCK_H2;
-        case '###':
-        case 'h3.':
-            return BLOCK_H3;
-        case '[]':
-        case '[ ]':
-            return BLOCK_CHECKLIST_ITEM;
-        case '---':
-        case '___':
-            return BLOCK_SEPARATOR;
-        default:
-            return null;
-    }
+const markdownSyntaxMapping = {
+    '*': BLOCK_LIST_ITEM,
+    '-': BLOCK_LIST_ITEM,
+    '+': BLOCK_LIST_ITEM,
+    '>': BLOCK_QUOTE,
+    '#': BLOCK_H1,
+    'h1.': BLOCK_H1,
+    '##': BLOCK_H2,
+    'h2.': BLOCK_H2,
+    '###': BLOCK_H3,
+    'h3.': BLOCK_H3,
+    '[]': BLOCK_CHECKLIST_ITEM,
+    '[ ]': BLOCK_CHECKLIST_ITEM,
+    '---': BLOCK_SEPARATOR,
+    ___: BLOCK_SEPARATOR,
 };
+
+const getMarkdownBlockFromPrefix = (
+    chars => markdownSyntaxMapping[chars] || null
+);
 
 export { getMarkdownBlockFromPrefix };
